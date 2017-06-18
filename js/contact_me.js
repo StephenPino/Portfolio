a8877529e7d9309f76b3774b6260346bb3781e22
Stephen Pino <spino725@gmail.com>
2017-06-18 02:37:07 -0500

    works

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://formspree.io/Stephen.Pino@utexas.edu",
                 type: "POST",
                 data: {
                     name: name,
